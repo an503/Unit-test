d510ad5597aa55cf7c36488173030090265891df
an503 <126401391+an503@users.noreply.github.com>
2023-04-06 12:45:08 +0300

    Add files via upload

---
+++ Unit-test/Test Chain/Test Chain/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_Chain
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
